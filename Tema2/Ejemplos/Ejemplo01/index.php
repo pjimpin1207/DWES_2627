27324a222ff4bbc2ba862dfa5c898c153e53cee8
Pablo Jimenez Pinto <pjimpin1207@g.educaand.es>
2026-09-23 12:58:27 +0200

    subir ejemplos

---
+++ Tema2/Ejemplos/Ejemplo01/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola Mundo PHP</title>
+</head>
+<body>
+    <?php
+        echo "<p>Hola Mundo  PHP</p>";
+    ?>
+</body>
+</html>
